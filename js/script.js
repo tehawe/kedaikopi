775c71f96a8fc3675a59dc7c96a6763704570ce2
tehawe <thw.teguh@gmail.com>
2023-08-23 18:07:58 +0700

    update fitur cari & shopping cart

--- js/script.js
+++ js/script.js
@@ -4,17 +4,44 @@ const navbarNav = document.querySelector('.navbar-nav');
 
 
 // Ketika Hamburger-Menu diklik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
-    return console.log('OK')
+    e.preventDefault();
 };
 
+// togle class active cari form
+const cari = document.querySelector('.cari');
+const cariBox = document.querySelector('#cari-box');
 
-// remove sidebar
+document.querySelector('#btn-cari').onclick = (e) => {
+    cari.classList.toggle('active');
+    cariBox.focus();
+    e.preventDefault();
+};
+
+// toggle class active keranjang
+const keranjang = document.querySelector('.keranjang');
+
+document.querySelector('#btn-keranjang').onclick = (e) => {
+    keranjang.classList.toggle('active');
+    e.preventDefault();
+};
+
+// remove elemen
 const sidebar = document.querySelector('#hamburger-menu');
+const btnCari = document.querySelector('#btn-cari');
+const btnKeranjang = document.querySelector('#btn-keranjang');
 
 document.addEventListener('click', function (e) {
     if (!sidebar.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
+    }
+
+    if (!btnCari.contains(e.target) && !cari.contains(e.target)) {
+        cari.classList.remove('active');
+    }
+
+    if (!btnKeranjang.contains(e.target) && !keranjang.contains(e.target)) {
+        keranjang.classList.remove('active');
     }
 });
